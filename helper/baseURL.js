a684ed4372f5337ddb140c32c30fb6e460f0bc30
abhinav Pandey <abhinav2598@gmail.com>
2020-12-25 15:24:38 +0530

    doness

--- helper/baseURL.js
+++ helper/baseURL.js
@@ -1,3 +1,3 @@
 //depending on the environment ki whether hamara project deployed h(production env) m h ya nhi we will decide the url
-const baseURL=process.env==='production'?"https://mystoreAbhi25.vercel.app":"http://localhost:3000";
+const baseURL=process.env==='production'?"":"http://localhost:3000";
 export default baseURL; 
